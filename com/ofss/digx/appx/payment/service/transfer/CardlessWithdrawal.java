9308982f2101d48dd383db06883049065b5db924
Mansoor Naseer <mansoor.naseer@hotmail.com>
2018-10-29 11:44:51 +0500

    PdfContentpublisher in fetchtransaction function

--- com/ofss/digx/appx/payment/service/transfer/CardlessWithdrawal.java
+++ com/ofss/digx/appx/payment/service/transfer/CardlessWithdrawal.java
@@ -2,8 +2,16 @@ package com.ofss.digx.appx.payment.service.transfer;
 
 import com.ofss.digx.app.context.ChannelContext;
 import com.ofss.digx.app.core.ChannelInteraction;
+import com.ofss.digx.app.dda.dto.statement.DemandDepositAccountActivityRequestDTO;
+import com.ofss.digx.app.dda.dto.statement.DemandDepositAccountActivityResponseDTO;
 import com.ofss.digx.appx.AbstractRESTApplication;
 import com.ofss.digx.appx.PATCH;
+import com.ofss.digx.appx.dda.service.DemandDeposit;
+import com.ofss.digx.appx.writer.content.PDFContentPublisher;
+import com.ofss.digx.datatype.CurrencyAmount;
+import com.ofss.digx.datatype.complex.Account;
+import com.ofss.digx.enumeration.accounts.TransactionType;
+import com.ofss.digx.enumeration.accounts.statement.SearchByType;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalCreateRequestDTO;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalCreateResponse;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalDTO;
@@ -11,7 +19,16 @@ import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalReadRe
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalReadResponse;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalResponse;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalUpdateRequestDTO;
+import com.ofss.fc.datatype.Date;
+import com.ofss.fc.framework.domain.common.dto.DataTransferObject;
 import com.ofss.fc.infra.log.impl.MultiEntityLogger;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
@@ -21,6 +38,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -224,4 +245,88 @@ public class CardlessWithdrawal
     Response response = null;
     return response;
   }
+
+  @GET
+  @Produces({"application/json"})
+  @Path("/{accountId}/transactions")
+  public Response fetchTransactions(@PathParam("accountId") Account accountId, @QueryParam("fromDate") Date fromDate, @QueryParam("toDate") Date toDate, @QueryParam("noOfTransactions") Integer lastNoOfTransactions, @QueryParam("transactionType") TransactionType transactionType, @QueryParam("searchBy") SearchByType searchCriteria, @QueryParam("fromAmount") String fromAmount, @QueryParam("toAmount") String toAmount, @QueryParam("referenceNo") String referenceNo, @QueryParam("narration") String narration)
+   {
+	  if (this.logger.isLoggable(Level.FINE)) {
+	      this.logger.log(Level.FINE, this.formatter.formatMessage("Entered into fetchTransactions in REST %s: accountId=%s, fromDate=%s, toDate=%s, lastNoOfTransactions=%s, searchCriteria=%s", new Object[] { THIS_COMPONENT_NAME, accountId, fromDate, toDate, lastNoOfTransactions, searchCriteria }));
+	    }
+	    ChannelContext channelContext = null;
+	    DemandDepositAccountActivityRequestDTO accountActivityRequestDTO = new DemandDepositAccountActivityRequestDTO();
+	    Response response = null;
+	    DemandDepositAccountActivityResponseDTO accountActivityResponse = null;
+	    ChannelInteraction channelInteraction = ChannelInteraction.getInstance();
+	    try
+	    {
+	      channelContext = super.getChannelContext();
+	      channelInteraction.begin(channelContext);
+	      accountActivityRequestDTO.setAccountId(accountId);
+	      accountActivityRequestDTO.setSearchByType(searchCriteria);
+	      accountActivityRequestDTO.setTransactionType(transactionType);
+	      accountActivityRequestDTO.setNoOfTransactions(lastNoOfTransactions);
+	      accountActivityRequestDTO.setFromDate(fromDate);
+	      accountActivityRequestDTO.setToDate(toDate);
+	      //accountActivityRequestDTO.setNarration(narration);
+	      if (fromAmount != null) {
+	        accountActivityRequestDTO.setFromAmount(new CurrencyAmount(new BigDecimal(fromAmount), null));
+	      }
+	      if (toAmount != null) {
+	        accountActivityRequestDTO.setToAmount(new CurrencyAmount(new BigDecimal(toAmount), null));
+	      }
+	      accountActivityRequestDTO.setReferenceNo(referenceNo);
+	      com.ofss.digx.app.dda.service.core.DemandDeposit demandDepositService = new com.ofss.digx.app.dda.service.core.DemandDeposit();
+	      accountActivityResponse = demandDepositService.fetchTransactions(channelContext.getSessionContext(), accountActivityRequestDTO);
+	      PDFContentPublisher pc = new PDFContentPublisher();
+	      try {
+			pc.publish((DataTransferObject)accountActivityResponse,new MediaType(),(MultivaluedMap<String, Object>)new MultivaluedHashMap(),(OutputStream)new FileOutputStream("resources"+System.currentTimeMillis()+".pdf"),new Locale("en"),"pdf");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	      response = buildResponse(accountActivityResponse, Response.Status.OK);
+	      try
+	      {
+	        channelInteraction.close(channelContext);
+	      }
+	      catch (com.ofss.digx.infra.exceptions.Exception e)
+	      {
+	        this.logger.log(Level.SEVERE, this.formatter
+	          .formatMessage("Error encountered while closing channelContext %s", new Object[] { channelContext }), e);
+	        
+	        response = buildResponse(e, Response.Status.INTERNAL_SERVER_ERROR);
+	      }
+	      if (!this.logger.isLoggable(Level.FINE)) {
+	    	  logger.log(Level.FINE, formatter.formatMessage("Entering send of merchantTransfer rest service, payment Id: %s", new Object[] { 123 }));
+	      }
+	    }
+	    catch (com.ofss.digx.infra.exceptions.Exception e)
+	    {
+	      this.logger.log(Level.SEVERE, this.formatter
+	        .formatMessage("Exception encountered while invoking the service %s for accountActivityRequestDTO=%s", new Object[] {DemandDeposit.class
+	        
+	        .getName(), accountActivityRequestDTO }), e);
+	      
+	      response = buildResponse(e, Response.Status.BAD_REQUEST);
+	    }
+	    finally
+	    {
+	      try
+	      {
+	        channelInteraction.close(channelContext);
+	      }
+	      catch (com.ofss.digx.infra.exceptions.Exception e)
+	      {
+	        this.logger.log(Level.SEVERE, this.formatter
+	          .formatMessage("Error encountered while closing channelContext %s", new Object[] { channelContext }), e);
+	        
+	        response = buildResponse(e, Response.Status.INTERNAL_SERVER_ERROR);
+	      }
+	    }
+	    this.logger.log(Level.FINE, this.formatter
+	      .formatMessage("Exiting fetchTransactions of Demand Deposit REST service, accountActivityResponse: %s", new Object[] { accountActivityResponse }));
+	    return response;
+	}
 }
--- com/ofss/digx/appx/payment/service/transfer/ICardlessWithdrawal.java
+++ com/ofss/digx/appx/payment/service/transfer/ICardlessWithdrawal.java
@@ -1,5 +1,9 @@
 package com.ofss.digx.appx.payment.service.transfer;
 
+import com.ofss.digx.datatype.complex.Account;
+import com.ofss.fc.datatype.Date;
+import com.ofss.digx.enumeration.accounts.TransactionType;
+import com.ofss.digx.enumeration.accounts.statement.SearchByType;
 import com.ofss.digx.sites.abl.app.payment.dto.transfer.CardlessWithdrawalDTO;
 import javax.ws.rs.core.Response;
 
@@ -12,6 +16,8 @@ public abstract interface ICardlessWithdrawal
   public abstract Response updateStatus(String paramString);
   
   public abstract Response delete(String paramString);
+  
+  public abstract Response fetchTransactions(Account paramAccount, Date paramDate1, Date paramDate2, Integer paramInteger, TransactionType paramTransactionType, SearchByType paramSearchByType, String paramString1, String paramString2, String paramString3, String paramString4);
   
   public abstract CardlessWithdrawalAuthentication cardlessWithdrawalAuthentication(String paramString);
 }
